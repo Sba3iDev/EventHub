58ad8432cf47f343ab29a7aa1ef3a6d1c0dce1d0
Sba3iDev <sba3idev@gmail.com>
2025-05-19 16:52:33 +0100

    Add user APIs

--- client/script/api.js
+++ client/script/api.js
@@ -37,3 +37,41 @@ export async function deleteEvent(id) {
         method: "DELETE",
     }).then(handleResponse);
 }
+
+export async function fetchUsers() {
+    return fetch(`${apiBase}users`).then(handleResponse);
+}
+
+export async function getUser(id) {
+    return fetch(`${apiBase}users/${id}`).then(handleResponse);
+}
+
+export async function registerUser(userData) {
+    return fetch(`${apiBase}users/register`, {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(userData),
+    }).then(handleResponse);
+}
+
+export async function loginUser(Userdata) {
+    return fetch(`${apiBase}users/login`, {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(Userdata),
+    }).then(handleResponse);
+}
+
+export async function updateUser(id, userData) {
+    return fetch(`${apiBase}users/${id}`, {
+        method: "PUT",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(userData),
+    }).then(handleResponse);
+}
+
+export async function deleteUser(id) {
+    return fetch(`${apiBase}users/${id}`, {
+        method: "DELETE",
+    }).then(handleResponse);
+}
